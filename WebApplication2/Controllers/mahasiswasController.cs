4efe74c6a656032e017b4f60a377b0390081c350
kutuonline <andrean.devz@gmail.com>
2017-09-16 13:35:32 +0700

    First commit for ASP NET Mahasiswa

---
+++ WebApplication2/Controllers/mahasiswasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class mahasiswasController : Controller
+    {
+        private db_kampusEntities db = new db_kampusEntities();
+
+        // GET: mahasiswas
+        public ActionResult Index()
+        {
+            return View(db.mahasiswas.ToList());
+        }
+
+        // GET: mahasiswas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mahasiswa mahasiswa = db.mahasiswas.Find(id);
+            if (mahasiswa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mahasiswa);
+        }
+
+        // GET: mahasiswas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: mahasiswas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nip,nm_mhs,email,alamat")] mahasiswa mahasiswa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.mahasiswas.Add(mahasiswa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mahasiswa);
+        }
+
+        // GET: mahasiswas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mahasiswa mahasiswa = db.mahasiswas.Find(id);
+            if (mahasiswa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mahasiswa);
+        }
+
+        // POST: mahasiswas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nip,nm_mhs,email,alamat")] mahasiswa mahasiswa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mahasiswa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mahasiswa);
+        }
+
+        // GET: mahasiswas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mahasiswa mahasiswa = db.mahasiswas.Find(id);
+            if (mahasiswa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mahasiswa);
+        }
+
+        // POST: mahasiswas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            mahasiswa mahasiswa = db.mahasiswas.Find(id);
+            db.mahasiswas.Remove(mahasiswa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
